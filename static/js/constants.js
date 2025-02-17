f3ba3106797455b1726f5e2274da1ec59f7b1475
nilay <nilayganatra1987@gmail.com>
2025-02-17 08:39:20 -0500

    tt

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://977a-142-180-220-42.ngrok-free.app/webhooks/rest/webhook";
+const rasa_server_url = "https://d1ea-142-180-220-42.ngrok-free.app/webhooks/rest/webhook";
 const sender_id = uuidv4();
